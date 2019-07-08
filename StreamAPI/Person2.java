0c274932637982e1a13f66a0916e666e4f7ea367
Evgenygroup <mailfrom61@gmail.com>
2019-07-08 17:17:41 +0200

    Stream homework

---
+++ StreamAPI/Person2.java
@@ -0,0 +1,20 @@
+import java.util.List;
+
+
+public class Person2 {
+    String name;
+    List<BankAccount2> bankAccounts;
+
+    public Person2(String name, List<BankAccount2> bankAccounts) {
+        this.name = name;
+        this.bankAccounts = bankAccounts;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<BankAccount2> getBankAccounts() {
+        return bankAccounts;
+    }
+}
