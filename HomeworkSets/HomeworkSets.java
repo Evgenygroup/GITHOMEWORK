626065aa16b03ca1f74ce98985e6bc49948f4bd5
Evgenygroup <mailfrom61@gmail.com>
2019-10-31 13:31:31 +0100

    homework sets

---
+++ HomeworkSets/HomeworkSets.java
@@ -0,0 +1,29 @@
+﻿
+
+import java.util.*;
+
+public class HomeworkSets {
+   /* 1.С использованием множеств (Set) реализовать функцию, которая вернет списокбез повторяющихся элементов:
+    List<String> removeDuplicates(List<String> input)
+    Пример:​ {“Ivan”, “Maria”, “Piotr”, “Anna”, “Maria”, “Ivan”} -> {“Ivan”, “Maria”, “Piotr”,“Anna”}
+    2.С использованием множеств реализовать функцию, которая вернетповторяющиеся элементы из данного списка:
+    List<String>
+    getDuplicates(List<String> input)
+    Пример: ​ {“Ivan”, “Maria”, “Piotr”, “Anna”, “Maria”, “Ivan”} -> {“Ivan”, “Maria”}*/
+
+    public List removeDuplicates(List<String> input) {
+        Set set = new HashSet(input);
+        return new ArrayList(set);
+    }
+
+    public List getDuplicates(List<String> input) {
+        Set set = new HashSet();
+        List duplicates = new ArrayList();
+        for (String s : input) {
+            if (!set.add(s)) duplicates.add(s);
+        }
+        return duplicates;
+
+    }
+
+}
